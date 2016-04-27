d4cf0fc1ed219b1ed92022d758b570bc875cac4e
Trevor Greenleaf <info@trevorgreenleaf.com>
2016-04-26 19:17:28 -0700

    added some magic code

--- wp-content/themes/miata_theme/header.php
+++ wp-content/themes/miata_theme/header.php
@@ -2,15 +2,12 @@
 <html>
 <head>
 	<title><?php if(is_home()) bloginfo('name'); else wp_title(''); ?></title>
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 	<link href='https://fonts.googleapis.com/css?family=Raleway:400,600,800,600italic,400italic,800italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
 	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
-
-
 	<?php wp_head();?>
 </head>
 <body>
