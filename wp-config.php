d33beab8cf7170aa2478f23da42b050000dee9af
Trevor Greenleaf <info@trevorgreenleaf.com>
2016-04-26 17:41:28 -0700

    changed db name to miatas with an s

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'miata');
+define('DB_NAME', 'miatas');
 
 /** MySQL database username */
 define('DB_USER', 'filmnerd');
